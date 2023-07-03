0cb3eb197e196c1d358c386d82c0af411fcdcc0b
sharvari-ghinmine <125306290+sharvari-ghinmine@users.noreply.github.com>
2023-07-03 19:51:33 +0530

    Add files via upload

---
+++ digitalclock.py
@@ -0,0 +1,16 @@
+
+
+from tkinter import *
+from tkinter.ttk import *
+
+from time import strftime
+root=Tk()
+root.title('Clock')
+
+
+
+def time()
+    string=strftime
+label=label(root, font=("ds-digital",80),background="black", foreground="cyan")
+label.pack(anchor='center')
+
